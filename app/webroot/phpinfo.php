df8fda4709e02e5a0057e4904cb79bc91ad8458d
Don <don@kindminds.com>
2013-09-27 15:39:53 +0800

    Add 'git pull' to phpinfo.php

--- app/webroot/phpinfo.php
+++ app/webroot/phpinfo.php
@@ -14,7 +14,30 @@
 		$pusher = @$payload['pusher']['name'];
 		$pushedRef = @$payload['ref'];
 		$pushedBranch = str_replace('refs/heads/','',$pushedRef);
+		echo 'pusher = '.$pusher.'<br>';
+		echo 'pwd = '.$pwd.'<br>';
+		echo 'pwd = '.$pwd.'<br>';
 	}
+
+	$gitPullResult = `git pull`;
+	$gitHardResetResult = `git reset --hard origin`;
+	$gitBranchResult = `git branch`;
+	// change permission for app/Console/cake
+	`chmod +x ../Console/cake`;
+	$bashExit = `exit`;
+	
+	$message = date(DATE_RFC822). "\n";
+	$message .= "pushedBranch: $pushedBranch\n";
+	$message .= "forcePerformPull: $forcePerformPull\n";
+	$message .= "git pull result: $gitPullResult";
+	$message .= "git hard reset: $gitHardResetResult";
+	$message .= "Current branch on server: $gitBranchResult";
+	if ($pusher) $message .= "Pusher: $pusher\n";
+	if ($pushedBranch) $message .= "To Branch: $pushedBranch\n";
+	$message .= "chmod +x ../Console/cake\n";
+	$message .= "Bash Exit.";
+
+	echo str_replace("\n",'<br />',$message);
 
 phpinfo();
 ?>
