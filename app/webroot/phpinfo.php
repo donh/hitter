66eacc7526edd92dd8571fe0f2a3fa0fa75759de
Don <don@kindminds.com>
2013-09-27 16:05:25 +0800

    Add Demo link to home.ctp

--- app/webroot/phpinfo.php
+++ app/webroot/phpinfo.php
@@ -1,4 +1,18 @@
 <?php
+/**
+* @php file name:	app/webroot/phpinfo.php
+* @description:		This php file receives webhook from Github and then performs git pull.
+* @related issues:	
+* @param:			void
+* @return:			void
+* @author:			Don Hsieh
+* @since:			09/27/2013
+* @last modified: 	09/27/2013
+* @called by:		Github webhook
+*/
+
+// https://help.github.com/articles/post-receive-hooks
+
 	echo date(DATE_RFC822).'<br>';//exit;
 	$me = `whoami`;
 	echo 'whoami = '.$me.'<br>';
@@ -11,9 +25,10 @@
 		if (isset($_REQUEST['payload'])) {
 			$payload = json_decode(stripslashes($_REQUEST['payload']), true);
 		}
+		var_dump($payload);
 		$pusher = @$payload['pusher']['name'];
 		$pushedRef = @$payload['ref'];
-		$pushedBranch = str_replace('refs/heads/','',$pushedRef);
+		$pushedBranch = str_replace('refs/heads/', '' ,$pushedRef);
 		echo 'pusher = '.$pusher.'<br>';
 		echo 'pwd = '.$pwd.'<br>';
 		echo 'pwd = '.$pwd.'<br>';
