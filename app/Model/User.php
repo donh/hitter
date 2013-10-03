273974ec08a9558c8c65f403fc6993c6ca9ae545
Don <don@kindminds.com>
2013-10-03 15:08:23 +0800

    Create users table in database

--- app/Model/User.php
+++ app/Model/User.php
@@ -12,6 +12,23 @@ class User extends AppModel {
 	* @param:			string $id
 	* @return:			array $user
 	* @author:			Don Hsieh
+	* @since:			10/03/2013
+	* @last modified: 	10/03/2013
+	* @called by:		
+	*/
+	public function getUserByUsername($username) {
+		$user = null
+		return $user;
+	}
+
+
+	/**
+	* @function name:	public function getUser($id)
+	* @description:		This function returns data of a user.
+	* @related issues:	QH-1
+	* @param:			string $id
+	* @return:			array $user
+	* @author:			Don Hsieh
 	* @since:			09/28/2013
 	* @last modified: 	09/28/2013
 	* @called by:		
